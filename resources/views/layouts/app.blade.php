7e2d30731c68136012f5476dc93ef85c5a99d5b5
G3org3B <vbt.george@gmail.com>
2019-02-27 23:30:47 +0100

    commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,33 +1,18 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <meta name="description" content=" @yield('meta_description', 'Boolean Careers - Corso Full Stack')">
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <title>@yield('title', 'Boolean Careers')</title>
-  </head>
-  <body>
-    <header>
-      <div class="header-left">
-        <img src="https://www.boolean.careers/images/common/logo.png" alt="Boolean Logo">
-      </div>
-      <div class="header-right">
-        <ul>
-          <li class="active">
-            <a href="{{ route('home') }}">Home</a>
-          </li>
-          <li>
-            <a href="{{ route('static.privacy' )}}">Privacy Policy</a>
-          </li>
-          <li>
-            <a href="{{ route('static.work' )}}">Lavora con noi</a>
-          </li>
-        </ul>
-      </div>
+    <head>
+        <meta charset="utf-8">
+        <meta name="description" content="@yield('meta_description', 'Boolean Careers - Corso Full Stack')">
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <title>@yield('title', 'Boolean Careers')</title>
+    </head>
+    <body>
+        @include('partials._header')
 
+        @yield('content')
 
-    </header>
+        @include('partials._footer')
 
-    @yield('content')
-  </body>
+        <script src="{{ asset('js/app.js') }}" charset="utf-8"></script>
+    </body>
 </html>
