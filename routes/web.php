648e9e13bba4dd40a637ecc72b3c32b95464acd1
G3org3B <vbt.george@gmail.com>
2019-02-27 23:27:57 +0100

    commit

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ Route::get('/', 'HomeController@index')->name('home');
 // pagine statiche
 Route::get('/privacy-policy','StaticPageController@privacyPolicy')->name('static.privacy');
 Route::get('/lavora-con-noi','StaticPageController@WorkWithUs')->name('static.work');
+
+//Pagine Studenti
+Route::get('/studenti', 'StudentController@index')->name('students.index');
+Route::get('/studenti/{slug}', 'StudentController@show')->name('students.show');
